517bbbf24b637ce32e77eaae0244f04e1061cd03
Piyushbhatt7 <vengersmon003@gmail.com>
2025-04-21 13:37:13 +0530

    main file

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        scaffoldBackgroundColor: AppColors.background
+        scaffoldBackgroundColor: AppColors.background,
       ),
       home: HomePage(),
     );
