d484b3c027f8edfa2ce985c5144df6f9b80b94eb
Piyushbhatt7 <vengersmon003@gmail.com>
2025-04-21 14:23:47 +0530

    side_bar

--- lib/Widgets/side_bar.dart
+++ lib/Widgets/side_bar.dart
@@ -28,22 +28,7 @@ class _SideBarState extends State<SideBar> {
 
              const SizedBox(height: 24.0, ),
 
-            Row(
-              mainAxisAlignment: isCollapsed ? MainAxisAlignment.center : MainAxisAlignment.start,
-              children: [
-                Container(
-                  margin: EdgeInsets.symmetric(vertical: 14.0, horizontal: 10.0),
-                  child: Icon(
-                  Icons.add,
-                   color: AppColors.iconGrey, 
-                   size: 22,
-                   ),
-                ),
-                isCollapsed ? 
-                const SizedBox()
-                : Text("Home", style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),)
-              ],
-            ), // 22
+            
 
             Container(
               margin: EdgeInsets.symmetric(vertical: 14.0, horizontal: 10.0),
