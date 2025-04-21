f5957d9ba4f2f281ae0601ce8f0ae4b98200d40d
Piyushbhatt7 <vengersmon003@gmail.com>
2025-04-21 15:02:12 +0530

    search section

---
+++ lib/Widgets/search_bar_button.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:perplexity_clone/Theme/colors.dart';
+
+class SearchBarButton extends StatefulWidget {
+  const SearchBarButton({super.key});
+
+  @override
+  State<SearchBarButton> createState() => _SearchBarButtonState();
+}
+
+class _SearchBarButtonState extends State<SearchBarButton> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(6.0),
+        color: Colors.transparent,
+      ),
+      child: Row(
+        children: [
+          Icon(Icons.plus_one, color: AppColors.iconGrey,)
+        ],
+      ),
+    );
+  }
+}
