93678b6d06ab1e1ef9e52c9599e29a1f2b8269a5
Piyushbhatt7 <vengersmon003@gmail.com>
2025-04-21 13:41:15 +0530

    main file

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:perplexity_clone/Pages/home_page.dart';
 import 'package:perplexity_clone/Theme/colors.dart';
 
@@ -16,6 +17,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         scaffoldBackgroundColor: AppColors.background,
+        textTheme: GoogleFonts.interTextTheme(),
       ),
       home: HomePage(),
     );
