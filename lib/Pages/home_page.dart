4fdbf836cbf38995560c5cdcc4b79f274bc6f7e9
Piyushbhatt7 <vengersmon003@gmail.com>
2025-04-21 12:48:55 +0530

    home page

--- lib/Pages/home_page.dart
+++ lib/Pages/home_page.dart
@@ -6,7 +6,9 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      body: Center(
+        child: Text("Hello World"),
+      ),
     );
   }
 }
