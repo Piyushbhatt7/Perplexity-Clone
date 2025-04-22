68edea8b4f14e4b0d0403a9de229ce0ffa178c65
Piyushbhatt7 <vengersmon003@gmail.com>
2025-04-22 19:16:37 +0530

    serch services

--- server/main.py
+++ server/main.py
@@ -13,6 +13,7 @@ search_service = SearchService()
 def chat_endpoint(body: ChatBody):
     # search the web and find appropriate sources
     search_results = search_service.web_search(body.query)
+    print(search_results)
     # sort the sources
     # generate the response using LLM
     
