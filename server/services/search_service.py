e69f02d2e60f55c3620dd286de6cd3347f5f93bb
Piyushbhatt7 <vengersmon003@gmail.com>
2025-04-22 10:57:52 +0530

    search services

--- server/services/search_service.py
+++ server/services/search_service.py
@@ -4,5 +4,5 @@ from config import Settings
 settings = Settings()
 
 class SearchService:
-    def web_search(self, query:str):
+    def web_search(self, query: str):
         print(Settings().TAVILY_API_KEY)
