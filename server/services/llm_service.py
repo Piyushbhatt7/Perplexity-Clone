c3b1d4c82cc6f3a4bbdae13462a65e73a98538c5
Piyushbhatt7 <vengersmon003@gmail.com>
2025-04-23 23:06:41 +0530

    llms

--- server/services/llm_service.py
+++ server/services/llm_service.py
@@ -33,5 +33,5 @@ class LLMService:
         
         response = self.model.generate_content(full_prompt)
         
-        return response.textm
-        
+        return response.text
+        # 1:45:29
