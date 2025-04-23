b830d10d7736293db9ff9a936098c564563b4490
Piyushbhatt7 <vengersmon003@gmail.com>
2025-04-23 11:11:36 +0530

    using google gemini for results

--- server/services/llm_service.py
+++ server/services/llm_service.py
@@ -1,6 +1,8 @@
+import google.generativeai as genai
+
 class LLMService:
     
     def __init__(self):
-        self.model = genai
+        self.model = genai.GenerativeModel("")
     def generate_response(self, query: str, search_results: list[dict]):
         pass
