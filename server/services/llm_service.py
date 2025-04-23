894d57a7894f67e109b2bf5b2ae5f2cd54170d47
Piyushbhatt7 <vengersmon003@gmail.com>
2025-04-23 13:08:24 +0530

    llm services

--- server/services/llm_service.py
+++ server/services/llm_service.py
@@ -1,8 +1,10 @@
 import google.generativeai as genai
+from config import Settings
 
+settings = Settings()
 class LLMService:
     
     def __init__(self):
-        self.model = genai.GenerativeModel("")
+        self.model = genai.GenerativeModel(settings.GEMINI_API_KEY)
     def generate_response(self, query: str, search_results: list[dict]):
-        pass
+        pass // 1:37
