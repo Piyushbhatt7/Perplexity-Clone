24b68a46280621c90ba9aeb7de1175ba012ba674
Piyushbhatt7 <vengersmon003@gmail.com>
2025-04-23 13:15:17 +0530

    llm services

--- server/services/llm_service.py
+++ server/services/llm_service.py
@@ -5,6 +5,7 @@ settings = Settings()
 class LLMService:
     
     def __init__(self):
-        self.model = genai.GenerativeModel(settings.GEMINI_API_KEY)
+        genai.configure(api_key=settings.GEMINI_API_KEY)
+        self.model = genai.GenerativeModel("gemini-2.0-flash-exp-image-generation")
     def generate_response(self, query: str, search_results: list[dict]):
         pass // 1:37
