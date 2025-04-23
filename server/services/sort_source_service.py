5bebf855a8dcfce0f289550cbff9db6b66f1273d
Piyushbhatt7 <vengersmon003@gmail.com>
2025-04-23 10:15:50 +0530

    sort services

--- server/services/sort_source_service.py
+++ server/services/sort_source_service.py
@@ -2,5 +2,9 @@ from typing import List
 
 
 class SortSourceService:
+    
+    def __init__(self):
+        self.embedding_model = SentenceTransformer("all-miniLM-L6-v2")
+    
     def sort_source(self, quey: str, search_results: List[dict]):
         
