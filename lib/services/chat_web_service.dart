74a368d3dfc6d6d7f27f1ca85e3322f983e22efa
Piyushbhatt7 <vengersmon003@gmail.com>
2025-04-24 11:15:22 +0530

    sending user data to get the gemai response

--- lib/services/chat_web_service.dart
+++ lib/services/chat_web_service.dart
@@ -5,12 +5,17 @@ class ChatWebService {
 
   WebSocket? _socket;
   void connect() {
-    _socket = WebSocket(Uri.parse("ws://localhost:8000/ws/chat"));
-
-    _socket!.messages.listen((message) {
-      final data = json.decode(message);
-      print(message);
+    WebSocket.connect("ws://localhost:8000/ws/chat").then((socket) {
+      _socket = socket;
+      _socket!.listen((message) {
+        final data = json.decode(message);
+        print(data['type']);
+      });
+    }).catchError((error) {
+      print("Failed to connect: $error");
     });
+
+    // Removed erroneous _socket!.messages.listen block as it is not defined for WebSocket.
   }
 
   void send()
