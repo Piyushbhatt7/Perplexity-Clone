422cfa97c3dcba50d7379d8e2f76f5e32c8bbf64
Piyushbhatt7 <vengersmon003@gmail.com>
2025-04-24 11:11:47 +0530

    sending user data to get the gemai response

--- lib/services/chat_web_service.dart
+++ lib/services/chat_web_service.dart
@@ -1,6 +1,16 @@
+import 'dart:convert';
+import 'dart:io';
+
 class ChatWebService {
+
+  WebSocket? _socket;
   void connect() {
-    
+    _socket = WebSocket(Uri.parse("ws://localhost:8000/ws/chat"));
+
+    _socket!.messages.listen((message) {
+      final data = json.decode(message);
+      print(message);
+    });
   }
 
   void send()
