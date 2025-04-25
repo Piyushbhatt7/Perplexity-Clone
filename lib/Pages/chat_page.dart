a97bc0620d124bf22ae07f945f6c8c471772d802
Piyushbhatt7 <vengersmon003@gmail.com>
2025-04-25 13:32:55 +0530

    chat page for better UI

--- lib/Pages/chat_page.dart
+++ lib/Pages/chat_page.dart
@@ -8,6 +8,21 @@ class ChatPage extends StatefulWidget {
 }
 
 class _ChatPageState extends State<ChatPage> {
+  // StreamBuilder(
+  //               stream: ChatWebService().contentStream,
+  //               builder: (context, snapahot)
+  //               {
+  //                 if(snapahot.connectionState == ConnectionState.waiting){
+  //                   return const Center(
+  //                     child: CircularProgressIndicator(),
+  //                   );
+  //                 }
+
+  //                 fullResponse += snapahot.data?['data'] ?? '';
+
+  //                 return Text(fullResponse);
+  //               }
+  //               ),
   @override
   Widget build(BuildContext context) {
     return Container();
