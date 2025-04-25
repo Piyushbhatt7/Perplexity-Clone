bd0997adada16c6fec6b0c44b4123cde7dc12ca8
Piyushbhatt7 <vengersmon003@gmail.com>
2025-04-25 13:20:40 +0530

    creating chat app to connect with perplexity

--- lib/Pages/home_page.dart
+++ lib/Pages/home_page.dart
@@ -13,6 +13,8 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
 
+  String fullResponse = "";
+
   @override
   void initState() {
     // TODO: implement initState
