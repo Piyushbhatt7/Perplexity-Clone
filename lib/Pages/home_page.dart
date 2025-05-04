dbe24759ad678cc827f5cd11d508867287d8d039
Piyushbhatt7 <vengersmon003@gmail.com>
2025-05-04 21:50:22 +0530

    home page

--- lib/Pages/home_page.dart
+++ lib/Pages/home_page.dart
@@ -14,7 +14,7 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {                                   
 
   @override
-  void initState() {
+  void initState() { 
     // TODO: implement initState
     super.initState();
     ChatWebService().connect(); 
